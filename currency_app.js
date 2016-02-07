8b77ba2e20c6b28733587d32be0da7e340eb80eb
clara <clara@clara.com>
2016-02-07 11:44:22 -0600

    added base template, stylesheets and images

--- currency_app.js
+++ currency_app.js
@@ -1,5 +1,8 @@
 var express = require('express');
+
 var routes = require('./routes/index');
+var about = require('./routes/about');
+
 var path = require('path');
 
 var app = express();
@@ -7,12 +10,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(express.static(path.join(__dirname, "static")));
+
+app.use('/', routes);
+app.use('/about', about);
+
+
+
 app.listen(3010, function() {
   console.log("Currency app listening on port 3010");
 });
 
-app.use('/', routes);
 module.exports = app
-
-
-
